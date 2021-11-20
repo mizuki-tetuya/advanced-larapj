55344cd6ae474a257a89d48752c312c2e68522b0
mizuki <mizukih130414@outlook.jp>
2021-11-20 10:38:32 +0900

    2021.11.20

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -9,7 +9,50 @@ class AuthorController extends Controller
 {
     public function index()
     {
-        $items = DB::select('select * from authors');
+        $items = DB::table('authors')->get(['name']);
         return view('index', ['items' => $items]);
     }
+    public function add()
+    {
+        return view('add');
+    }
+    public function create(Request $request)
+    {
+        $param = [
+            'name' => $request->name,
+            'age' => $request->age,
+            'nationality' => $request->nationality,
+        ];
+        DB::insert('insert into authors (name, age, nationality) values (:name, :age, :nationality)', $param);
+        return redirect('/');
+    }
+    public function edit(Request $request)
+   {
+       $param = ['id' => $request->id];
+       $item = DB::select('select * from authors where id = :id', $param);
+       return view('edit', ['form' => $item[0]]);
+   }
+   public function update(Request $request)
+   {
+       $param = [
+           'id' => $request->id,
+           'name' => $request->name,
+           'age' => $request->age,
+           'nationality' => $request->nationality,
+       ];
+       DB::update('update authors set name =:name, age =:age, nationality =:nationality where id =:id', $param );
+       return redirect('/');
+   }
+   public function delete(Request $request)
+   {
+       $param = ['id' => $request->id];
+       $item = DB::select('select * from authors where id = :id', $param);
+       return view('delete', ['form' => $item[0]]);
+   }
+   public function remove(Request $request)
+   {
+       $param = ['id' => $request->id];
+        DB::delete('delete from authors where id =:id', $param);
+       return redirect('/');
+   }
 }
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,11 +1,10 @@
 <!DOCTYPE html>
 <html lang="ja">
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>@yield('title')</title>
-  <style>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title')</title>
+    <style>
     body {
       font-size:16px;
       margin: 5px;
@@ -15,17 +14,17 @@
       color:white;
       text-shadow:1px 0 5px #289ADC;
       letter-spacing:-4px;
-      margin-left: 10px;
+      margin-left: 10px
     }
     .content {
-      margin: 10px;
+      margin:10px; 
     }
-  </style>
-</head>
-<body>
-  <h1>@yield('title')</h1>
-  <div class="content">
+    </style>
+  </head>
+  <body>
+    <h1>@yield('title')</h1>
+    <div class="content">
     @yield('content')
-  </div>
+    </div>
 </body>
 </html>
